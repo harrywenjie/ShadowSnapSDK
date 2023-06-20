2092ca272aadc06d7903a6962b96e86dd2eddcf8
Harry <harrywenjie@gmail.com>
2023-06-20 23:14:46 +1200

    0.0.2

---
+++ Sources/ShadowSnapSDK.xcframework/ios-arm64/ShadowSnapSDK.framework/Headers/ShadowSnapSDK.h
@@ -0,0 +1,18 @@
+//
+//  ShadowSnapSDK.h
+//  ShadowSnapSDK
+//
+//  Created by Harry He on 20/06/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ShadowSnapSDK.
+FOUNDATION_EXPORT double ShadowSnapSDKVersionNumber;
+
+//! Project version string for ShadowSnapSDK.
+FOUNDATION_EXPORT const unsigned char ShadowSnapSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShadowSnapSDK/PublicHeader.h>
+
+
