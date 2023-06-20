8f1fa490c76b2c41331e1ef5fefaac985b70f1de
Harry <harrywenjie@gmail.com>
2023-06-20 23:24:29 +1200

    0.0.3

--- Package.swift
+++ Package.swift
@@ -19,6 +19,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "ShadowSnapSDK",
-            path: "/Sources/ShadowSnapSDK.xcframework")
+            path: "./Sources/ShadowSnapSDK.xcframework")
     ]
 )
